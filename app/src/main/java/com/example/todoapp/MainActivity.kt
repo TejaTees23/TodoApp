0b9fd1afe031318232bad65404c142d396f0aa2a
d3905212 <d3905212@tees.ac.uk>
2024-04-26 14:26:30 +0100

    Still missing references to drawables

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -8,24 +8,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.composableLambda
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
+//    @Composable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
         setContent {
 
-//            val navController = remenberNavController()
-//            NavHost(navController = navController,startDescription = "LoginScreen.kt",builder = {
-//                composable("LoginScreen){
-//
+           val navController = rememberNavController()
+//           NavHost(navController = navController,startDescription = "LoginScreen.kt",builder = {
+//               composable("LoginScreen")
+                       LoginScreen(navController)
 //                }
-//            })
-            TodoAppTheme {
+//            }
+
+            TodoAppTheme{
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
